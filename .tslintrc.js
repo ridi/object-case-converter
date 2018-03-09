e659b33f00effd463518a454b34f87b85e96ddb3
Heejae Jang <heejae@ridi.com>
2018-03-09 15:16:21 +0900

    Remove `@ridi/tslint-config` from '@ridi/object-case-converter`

--- .tslintrc.js
+++ .tslintrc.js
@@ -1,8 +1,17 @@
 module.exports = {
   'extends': [
-    '@ridi/tslint-config',
+    'tslint:recommended',
   ],
-  'rules': {},
+  'rules': {
+    'interface-name': false,
+    'no-implicit-dependencies': false,
+    'no-submodule-imports': false,
+    'variable-name': [true, 'allow-pascal-case'],
+    'semicolon': [true, 'always'],
+    'quotemark': [true, 'single', 'jsx-double'],
+    'jsx-no-multiline-js': false,
+    'object-literal-sort-keys': false,
+  },
   'linterOptions': {
     'exclude': [
       '**/node_modules/**',
